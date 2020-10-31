561c40fb432812518b37d75b8068cd4d78bc35ca
Sarthak Sharma <freelancesarthak@gmail.com>
2020-10-31 15:02:29 +0530

    CMT #3: Introduction to Templates
    Successful implementation of few functions of List class.
    TODO: Throw exception in at( ) function.

--- driver.cpp
+++ driver.cpp
@@ -1,20 +1,31 @@
 #include <iostream>
+#include <string>
 #include "include/ds.h"
 
 int main() {
 	std::cout << "Driver Code" << std::endl;
 
-	List<int> newList(5);
-	newList.insert(4);
-	newList.display();
-	newList.insert(8);
-	newList.display();
-	newList.insert(9);
-	newList.display();
-	newList.insert(10);
-	newList.display();
-	newList.insert(15);
+	List<int> newList(34);
+	newList.insert(674);
+	newList.insert_beg(6855);
+	newList.insert_at(42, 0);
+	newList.insert_at(87, newList.length());
+	newList.insert_at(534, 8);
 	newList.display();
 
+	List<int> secondList(45);
+	secondList.insert(454);
+	secondList.insert(78);
+	secondList.insert(204);
+	secondList.insert(984);
+	secondList.insert(114);
+	std::cout <<"Length of list before join "<< newList.length() << std::endl;
+	std::cout << "Size of the list before join is: " << newList.size() << " bytes" << std::endl;
+	secondList.display();
+	newList.join(secondList);
+	newList.display();
+
+	std::cout << "Length of list after join " << newList.length() << std::endl;
+	std::cout << "Size of the list before join is: " << newList.size() << " bytes" << std::endl;
 	return 0;
 }
--- include/LINKED_LIST/list.h
+++ include/LINKED_LIST/list.h
@@ -1,5 +1,6 @@
 #pragma once
 #include<iostream>
+#include<exception>
 
 /*
 template <class Vector>
@@ -17,13 +18,13 @@ template <class T>
 class List
 {
 public:
-	List<T>() : m_size(0), m_start(nullptr), m_end(nullptr) {}
+	List<T>() : m_length(0), m_start(nullptr), m_end(nullptr) {}
 	List<T>(T input_data) {
 		Node* newNode = new Node;
 		newNode->node_data = input_data;
 		newNode->next_node = nullptr;
-		m_start = newNode;
-		m_size++;
+		m_start = m_end = newNode;
+		m_length = 1;
 	}
 	~List<T>() {};
 
@@ -37,7 +38,7 @@ private:
 		Node* next_node;
 
 	public:
-		Node(): node_data(NULL), next_node(nullptr){}
+		Node(): node_data(), next_node(nullptr){}
 		Node(T input_data, Node* new_next_node = nullptr) {
 			node_data = input_data;
 			next_node = new_next_node;
@@ -52,22 +53,28 @@ private:
 		//void set_next(Node* new_next_node) : next_node(new_next_node) {}
 	};
 
-	unsigned int m_size;
+	unsigned int m_length;
 	Node* m_start;
 	Node* m_end;
 
-	Node* get_node(int index) {
-		if (index <= 0) return m_start;
-		else if (index >= m_size) return m_end;
+	Node* get_node(unsigned int index) {
+		if (index < 0 || index > m_length) return nullptr;
 		else {
-			Node* m_traveler;
-			m_traveler = m_start;
+			Node* m_traveler = m_start;
 			unsigned int counter = 0;
-			while (counter != index) {
+			while (m_traveler != nullptr) {
+				if (counter == index) break;
 				m_traveler = m_traveler->next_node;
+				counter++;
 			}
 			return m_traveler;
 		}
+	}
+	Node* get_new_node(T input_data) {
+		Node* newNode = new Node;
+		newNode->node_data = input_data;
+		newNode->next_node = nullptr;
+		return newNode;
 	}
 
 public:
@@ -75,16 +82,16 @@ public:
 	//iterator<List> begin();
 	//iterator<List> end();
 
-	T at(int index);
+	T at(unsigned int index);
 	void clear();
 	void display();
-	void display_from(int starting_index);
+	void display_from(unsigned int starting_index);
 	void insert(T input_data);
 	int find(T input_data);
 	void insert_beg(T input_data);
-	void insert_at(T input_data, int index);
+	void insert_at(T input_data, unsigned int index);
 	bool isEmpty();
-	void join(List<T>*);
+	void join(List<T>);
 	unsigned int length();
 	T peekFirst();
 	T peekMiddle();
@@ -95,7 +102,9 @@ public:
 	void remove_beg();
 	void reverse();
 	void shuffle();
+	int size();
 	void sort();
+	List<T>* split(int split_from_index);
 };
 
 // Implementations go below
@@ -113,13 +122,29 @@ iterator List<T>::end() {
 */
 
 template <typename T>
-T List<T>::at(int index) {
-
+T List<T>::at(unsigned int index) {
+	std::string IOOR = "Index_Out_of_Range: ";
+	try {
+		if (index > m_length)
+			throw IOOR;
+	}
+	catch (std::string e) {
+		std::cout << e << "Returning value at end." << std::endl;
+		return m_end->node_data;
+	}
+	return get_node(index)->node_data;
 }
 
 template <typename T>
 void List<T>::clear() {
-
+	Node* node = m_start;
+	Node* temporary = node;
+	while (temporary != nullptr) {
+		temporary = node->next_node;
+		delete node;
+		node = temporary;
+		m_length--;
+	}
 }
 
 template <typename T>
@@ -129,50 +154,101 @@ void List<T>::display() {
 		std::cout << newNode->node_data << " -> ";
 		newNode = newNode->next_node;
 	}
+	std::cout << "NULL" << std::endl;
 }
 
 template <typename T>
-void List<T>::display_from(int starting_index) {
-
+void List<T>::display_from(unsigned int starting_index) {
+	if (starting_index > m_length)
+		std::cout << "NULL" << std::endl;
+	else {
+		Node* traveler = get_node(starting_index);
+		while (traveler != nullptr) {
+			std::cout << traveler->node_data << " -> ";
+			traveler = traveler->next_node;
+		}
+		std::cout << "NULL" << std::endl;
+	}
 }
 
 template <typename T>
 int List<T>::find(T input_data) {
+	// TODO: Currently O(n), later implement using sort() and performing binary search.
 
+	Node* traveler = m_start;
+	int location = 0;
+	while (traveler != nullptr) {
+		if (traveler->node_data == input_data)
+			return location;
+		traveler = traveler->next_node;
+		location++;
+	}
+	return -1;
 }
 
 template <typename T>
 void List<T>::insert(T input_data) {
-	Node* newNode = new Node;
-	newNode->node_data = input_data;
-	newNode->next_node = nullptr;
-	m_start->next_node = newNode;
-	m_size++;
+	Node* newNode = get_new_node(input_data);
+
+	if (m_length == 0) {
+		m_start = m_end = newNode;
+		m_length++;
+	}
+	else {
+		m_end->next_node = newNode;
+		m_end = newNode;
+		m_length++;
+	}
 }
 
 template <typename T>
 void List<T>::insert_beg(T input_data) {
-
+	Node* newNode = get_new_node(input_data);
+	if (m_length == 0) {
+		m_start = m_end = newNode;
+		m_length++;
+	}
+	else {
+		newNode->next_node = m_start;
+		m_start = newNode;
+		m_length++;
+	}
 }
 
 template <typename T>
-void List<T>::insert_at(T input_data, int index) {
+void List<T>::insert_at(T input_data, unsigned int index) {
+	
+	if (index <= 0) insert_beg(input_data);
+	else if (index >= m_length) insert(input_data);
+	else {
+		Node* node_to_shift = get_node(index);
+		Node* previous = get_node(--index);
+		Node* newNode = get_new_node(input_data);
 
+		previous->next_node = newNode;
+		newNode->next_node = node_to_shift;
+		m_length++;
+	}
 }
 
 template <typename T>
 bool List<T>::isEmpty() {
-	return (m_size == 0);
+	return (m_length == 0);
 }
 
 template <typename T>
-void List<T>::join(List* second_list) {
-
+void List<T>::join(List second_list) {
+	int length = second_list.length();
+	int local_counter = 0;
+	while (local_counter < length) {
+		insert(second_list.at(local_counter));
+		local_counter++;
+	}
 }
 
 template <typename T>
 unsigned int List<T>::length() {
-	return m_size;
+	return m_length;
 }
 
 template <typename T>
@@ -221,6 +297,18 @@ void List<T>::shuffle() {
 }
 
 template <typename T>
+int List<T>::size() {
+	return sizeof(m_start) * m_length;
+}
+
+template <typename T>
 void List<T>::sort() {
 
+}
+
+template <typename T>
+List<T>* split(int split_from_index) {
+	List<T>* split = new List;
+
+	return split;
 }
